f60dfb8538304067232622d3593440a0abba48ff
Suryateja Kesireddy <ksuryateja170@gmail.com>
2026-08-08 22:55:08 +0530

    fix: add user phone migration

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,5 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { defineConfig } from "prisma/config";
+
+dotenv.config({
+    path: ".env.aiven",
+});
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
